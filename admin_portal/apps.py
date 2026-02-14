ac111f284675e49a3ee66fa4051eae2dab05e12c
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2026-02-14 15:15:14 +0100

    Add Autodiscover for Admin Menus
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- admin_portal/apps.py
+++ admin_portal/apps.py
@@ -1,5 +1,16 @@
-from django.apps import AppConfig
+from django.apps import AppConfig, apps
 
 
 class AdminPortalConfig(AppConfig):
     name = 'admin_portal'
+    verbose_name = 'Administration Portal'
+
+    def ready(self) -> None:
+        self.autodiscover_menus()
+
+    def autodiscover_menus(self) -> None:
+        for app_config in apps.get_app_configs():
+            try:
+                __import__(f"{app_config.name}.admin_menu")
+            except ModuleNotFoundError:
+                pass
